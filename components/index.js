0898c3d5aa79a465388b72db206d561361c1124c
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-03-28 20:34:57 -0700

    Added ActivityMakerCard.jsx

--- components/index.js
+++ components/index.js
@@ -3,11 +3,13 @@ import ActivityCard from './ActivityCard';
 import Day from './Day';
 import Activities from './Activities';
 import ActivityMaker from './ActivityMaker';
+import ActivityMakerCard from './ActivityMakerCard';
 
 export {
   Calendar,
   ActivityCard,
   Day,
   Activities,
-  ActivityMaker
+  ActivityMaker,
+  ActivityMakerCard
 };
