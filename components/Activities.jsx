0c90014e4406fdd5b991d0eafbb64899f33bbb97
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-01 14:28:42 -0700

    added dynamic activities list to Activities

--- components/Activities.jsx
+++ components/Activities.jsx
@@ -4,21 +4,56 @@ import Day from './Day';
 
 const Activities = () => {
 
-    
-
+    const activities = [
+        {
+            id: "1",
+            start: "2",
+            end: "3",
+            title: "Work in the project", 
+            description: "Indeed, i do need to work on it"
+        },
+        {
+            id: "2",
+            start: "2",
+            end: "3",
+            title: "Work in the project", 
+            description: "Indeed, i do need to work on it"
+        },
+        {
+            id: "3",
+            start: "2",
+            end: "3",
+            title: "Work in the project", 
+            description: "Indeed, i do need to work on it"
+        }
+    ]
+    /*
+    <Day/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <Day/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <Day/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+    */
     return(
         <View>
 
             <Day/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <Day/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <Day/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
-            <ActivityCard start="2" end="4" title="Work in the project" description="Indeed, i do need to work on it"/>
+            {activities.map((activity) => {
+                return (
+                    <ActivityCard
+                        start={activity.start} 
+                        end={activity.end}
+                        title={activity.title}
+                        description={activity.description}
+                        key={activity.id}
+                    />
+                );
+            })}
 
         </View>
     );
