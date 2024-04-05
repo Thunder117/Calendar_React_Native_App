8d8cdd7a9aa00855da35666ac9a439f293890e86
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-05 11:30:39 -0700

    added Month.jsx functionality

--- components/Activities.jsx
+++ components/Activities.jsx
@@ -6,14 +6,29 @@ import Month from './Month';
 const Activities = (props) => {
     let previousDate = 0;
     let previousMonth = 0;
-    /*if(activity.date.slice(5,7) == previousMonth) {
-                    <Month date={activity.date} key={previousMonth}/>
-                }*/
 
     return(
         <View>
             
             {props.activities.map((activity) => {
+
+                if(activity.date.slice(5,7) != previousMonth) {
+                    previousDate = activity.date.slice(-2);
+                    previousMonth = activity.date.slice(5,7);
+                    return(
+                        <View key={activity.date}>
+                            <Month date={activity.date} key={previousMonth}/>
+                            <Day date={activity.date} key={activity.date}/>
+                            <ActivityCard
+                                start={activity.start} 
+                                end={activity.end}
+                                title={activity.title}
+                                description={activity.description}
+                                key={activity.id}
+                            />
+                        </View>
+                    );
+                } 
 
                 if(activity.date.slice(-2) != previousDate) {
                     previousDate = activity.date.slice(-2);
