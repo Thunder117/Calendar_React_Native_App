452d34f7e5ff5569b9ff8e1c0370c89dbcf33970
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-04 23:18:57 -0700

    added days of the week to Day

--- components/Day.jsx
+++ components/Day.jsx
@@ -2,15 +2,18 @@ import { View, Text } from 'react-native'
 import styles from './styles';
 
 const Day = (props) => {
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const date = new Date();
     const day = date.getDate();
-    const month = date.toLocaleString('en', { month: 'long' });
-    
+    const monthString = date.toLocaleString('en', { month: 'long' });
+    const monthNumber = date.getMonth();
+    const dayOfWeek = (new Date(`2024-${monthNumber+1}-${props.day}T00:00:00`)).getDay();
+   
     return(
         <View style={styles.dayContainer}> 
             <Text style={styles.dayText}>
                 { day == props.day && <Text style={styles.dayHighlight}>Today, </Text> }
-                {props.day} of {month}
+                {weekday[dayOfWeek]} {props.day} of {monthString}
             </Text>
         </View>
     );
