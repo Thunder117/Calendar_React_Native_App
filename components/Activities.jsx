d6a739cc598312df8c3b59feaf02de8bc0e22cee
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-05 12:35:14 -0700

    Added Year.jsx and added functionality to Activities.jsx

--- components/Activities.jsx
+++ components/Activities.jsx
@@ -2,19 +2,44 @@ import { View } from 'react-native'
 import ActivityCard from './ActivityCard';
 import Day from './Day';
 import Month from './Month';
+import Year from './Year';
 
 const Activities = (props) => {
     let previousDate = 0;
     let previousMonth = 0;
+    let previousYear = 0;
 
     return(
         <View>
             
             {props.activities.map((activity) => {
+                const currentDate = activity.date.slice(-2);
+                const currentMonth = activity.date.slice(5,7);
+                const currentYear = activity.date.slice(0,4);
 
-                if(activity.date.slice(5,7) != previousMonth) {
-                    previousDate = activity.date.slice(-2);
-                    previousMonth = activity.date.slice(5,7);
+                if(currentYear != previousYear) {
+                    previousDate = currentDate;
+                    previousMonth = currentMonth;
+                    previousYear = currentYear;
+                    return(
+                        <View key={activity.date}>
+                            <Year date={activity.date} key={previousYear}/>
+                            <Month date={activity.date} key={previousMonth}/>
+                            <Day date={activity.date} key={activity.date}/>
+                            <ActivityCard
+                                start={activity.start} 
+                                end={activity.end}
+                                title={activity.title}
+                                description={activity.description}
+                                key={activity.id}
+                            />
+                        </View>
+                    );
+                }
+
+                if(currentMonth != previousMonth) {
+                    previousDate = currentDate;
+                    previousMonth = currentMonth;
                     return(
                         <View key={activity.date}>
                             <Month date={activity.date} key={previousMonth}/>
@@ -30,8 +55,8 @@ const Activities = (props) => {
                     );
                 } 
 
-                if(activity.date.slice(-2) != previousDate) {
-                    previousDate = activity.date.slice(-2);
+                if(currentDate != previousDate) {
+                    previousDate = currentDate;
                     return(
                         <View key={activity.date}>
                             <Day date={activity.date} key={activity.date}/>
---
+++ components/Year.jsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import styles from './styles';
+
+const Year = (props) => {
+    const dateInString = props.date // Date passed by parent in string
+    const dateInNumberYear = dateInString.slice(0,4); // Year passed by parent in number
+
+    console.log(dateInNumberYear)
+    return(
+        <View style={styles.dayContainer}> 
+
+            <Text>
+                { dateInNumberYear }
+            </Text>
+
+        </View>
+    );
+}
+
+export default Year
